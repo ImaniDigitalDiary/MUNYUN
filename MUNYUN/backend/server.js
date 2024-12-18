f0d90eb7a0dc1bf594cc4b0a1b1f890e75c7d88e
TechGirlMani <techgirlmani@gmail.com>
2024-12-18 09:41:41 -0500

    imported dotenv

--- MUNYUN/backend/server.js
+++ MUNYUN/backend/server.js
@@ -1,14 +1,18 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+// import { connectDB } from './config/db.js';
 
+dotenv.config();
 
 const app = express();
 
 // 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
+app.get('/expenses', (req, res) => {})
 
+console.log(process.env.MONGO_URI);
 app.listen(8000, () => {
+    // connectDB();
     console.log('Server started at http://localhost:8000')
 })
+
