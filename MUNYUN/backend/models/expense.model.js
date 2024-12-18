e5f250d08ecad0a5ee75e32acbfddc104b5da9a7
TechGirlMani <techgirlmani@gmail.com>
2024-12-18 09:55:07 -0500

    created expense model

---
+++ MUNYUN/backend/models/expense.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const expenseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }, 
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    // createdAt, updatedAt
+    timestamps: true
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+export default Expense;
