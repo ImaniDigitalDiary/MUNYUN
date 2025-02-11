474c10acd165989ca6abb2b9bbbdbdd3b1309d3e
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-02-10 23:31:12 -0500

    added new route for reporting

--- MUNYUN/backend/routes/expense.route.js
+++ MUNYUN/backend/routes/expense.route.js
@@ -1,9 +1,6 @@
 import express from 'express';
 
-
-
-
-import { createExpense, deleteExpense, getExpenses, updateExpense } from '../controllers/expense.controller.js';
+import { createExpense, deleteExpense, getExpenses, updateExpense, updateCategory } from '../controllers/expense.controller.js';
 
 const router = express.Router();
 
@@ -18,5 +15,11 @@ router.put('/:id', updateExpense)
 
 // DELETE AN EXPENSE
 router.delete('/:id', deleteExpense ) //:id means it will be dynamic and can be any value the user passes
+
+// Update expense category
+router.put('/update-category/:id', updateCategory)
+
+// Reporting route to retrieve aggregated data for charting purposes
+router.get('/report', getExpenseReport)
 
 export default router;
