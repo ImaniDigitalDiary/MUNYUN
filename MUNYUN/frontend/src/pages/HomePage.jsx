b099cd3db8e8420e61466643cd56841988cdfc1c
TechGirlMani <ImaniDigitalDiary@gmail.com>
2025-02-13 11:12:16 -0500

    added spin animation to flowers so that they rotate on loop

--- MUNYUN/frontend/src/pages/HomePage.jsx
+++ MUNYUN/frontend/src/pages/HomePage.jsx
@@ -15,30 +15,34 @@ function HomePage() {
     <div className='homeMainCont'>
     <Navbar />
     <div className='homePage'>
-      <div className="div1">
-        <img src={PinkFlower}/>
-        <img src={PinkFlower}/>
-        <img src={PinkFlower}/>
-        {/* <Box background='pink.400' width='100%' padding={4} color='white'>
-          Box 1
-        </Box>
-        <Box background='pink.400' width='100%' padding={4} color='white' >
-          Box 2
-        </Box>
-        <Box background='pink.400' width='100%' padding={4} color='white' >
-          Box 3
-        </Box>
-        <Box background='pink.400' width='100%' padding={4} color='white' >
-          Box 4
-        </Box>
-        <Box background='pink.400' width='100%' padding={4} color='white'>
-          Box 5
-        </Box> */}
+      <div className='div1'>
+        <div className="flowerCont">
+          <img src={PinkFlower} className='flower'/>
+          <a href="link.html" class="link">Click link</a>
+        </div>
+        <div className="flowerCont">
+          <img src={PinkFlower} className='flower'/>
+          <a href="link.html" class="link">Click link</a>
+        </div>
+        <div className="flowerCont">
+          <img src={PinkFlower} className='flower'/>
+          <a href="link.html" class="link">Click link</a>
+        </div>
+        <div className="flowerCont">
+          <img src={PinkFlower} className='flower'/>
+          <a href="link.html" class="link">Click link</a>
+        </div>
       </div>
+      
+
       <div className="div2">Div 2</div>
+
       <div className="div3">Div 3</div>
+
       <div className="div4">Expense Report<Reports /></div>
+
       <div className="div5">Div 5</div>
+
       <div className="div6">Div 6</div>
     </div>
     </div>
